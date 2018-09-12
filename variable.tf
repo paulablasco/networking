db4281c49583039e5686886b6dd53d0c5ef26b13
Paula <paula@MBPaula.local>
2018-09-12 23:41:24 +0200

    initial commit

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+  description = "Name of the Azure subscription ID"
+}
+
+variable "client_id" {
+  description = "Name of the Azure client_id"
+}
+
+variable "client_secret" {
+  description = "Name of the Azure client_secret"
+}
+
+variable "tenant_id" {
+  description = "Name of the Azure tenant_id"
+}
